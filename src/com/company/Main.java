d0b46287b73ad905e1b0e8a4bce82ada75b6235c
esrefozguraltuntas <esrefozguraltuntas@gmail.com>
2021-11-02 22:05:18 -0700

    Initial clean code commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Helloworld");
-        System.out.println("Cleand code here");
+        System.out.println("Clean code here");
+        System.out.println("Clean code saving here");
     }
 }
